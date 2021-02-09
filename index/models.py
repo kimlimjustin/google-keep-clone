9b62c8dd54c462254aa617805aa863e5bbb1f694
kimlimjustin <kimlimjustin@gmail.com>
2021-02-09 11:56:56 +0700

    Added basic layout

--- index/models.py
+++ index/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser, models.Model):
     email = models.EmailField(unique=True)
+    UItheme = models.CharField(default = "dark", max_length=10)
+    view = models.CharField(default="grid", max_length=10)
